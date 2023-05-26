1e2272e46fc3ed03df78b3d91b94faff03edb8fe
misspia <pia@commandbar.com>
2023-05-26 10:25:56 -0400

    setup sections

---
+++ src/Hero/index.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+import { Section } from "../components";
+
+const Container = styled(Section)``;
+export const Hero = () => {
+  return <Container>hero</Container>;
+};
---
+++ src/Projects/index.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+import { Section } from "../components";
+
+const Container = styled(Section)``;
+export const Projects = () => {
+  return <Container>projects</Container>;
+};
