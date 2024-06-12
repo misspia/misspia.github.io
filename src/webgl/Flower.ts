9a9b850d3f0603f20a6ad7fb616608bca6658809
misspia <pia_leung@hotmail.com>
2024-06-11 23:48:37 -0400

    init flower petal positions and rotations

--- src/webgl/Flower.ts
+++ src/webgl/Flower.ts
@@ -1,20 +1,59 @@
 import * as THREE from "three";
+import { randomFloatBetween } from "@utils";
 
-const NUM_PETALS = 5;
+const MIN_X_POS = -5;
+const MAX_X_POS = 5;
+const MIN_Z_POS = -5;
+const MAX_Z_POS = 5;
+
+const PETAL_DIST_FROM_ORIGIN = 0.5;
 
 // https://x.com/kchplr/status/1624137747039457284
 export class Flower {
+  origin: THREE.Vector3;
   positions: number[];
   rotations: number[];
 
   constructor() {
+    this.origin = new THREE.Vector3(
+      randomFloatBetween(MIN_X_POS, MAX_X_POS),
+      0,
+      randomFloatBetween(MIN_Z_POS, MAX_Z_POS),
+    );
     this.positions = [];
     this.rotations = [];
     this.createFlowerPetals();
   }
 
   createFlowerPetals() {
-    for (let i = 0; i < NUM_PETALS; i++) {}
+    this.positions.push(
+      this.origin.x + PETAL_DIST_FROM_ORIGIN,
+      this.origin.y,
+      this.origin.z,
+      this.origin.x + PETAL_DIST_FROM_ORIGIN,
+      this.origin.y,
+      this.origin.z + PETAL_DIST_FROM_ORIGIN,
+      this.origin.x - PETAL_DIST_FROM_ORIGIN,
+      this.origin.y,
+      this.origin.z + PETAL_DIST_FROM_ORIGIN,
+      this.origin.x - PETAL_DIST_FROM_ORIGIN,
+      this.origin.y,
+      this.origin.z - PETAL_DIST_FROM_ORIGIN,
+      this.origin.x - PETAL_DIST_FROM_ORIGIN,
+      this.origin.y,
+      this.origin.z,
+      this.origin.x,
+      this.origin.y,
+      this.origin.z + PETAL_DIST_FROM_ORIGIN,
+      this.origin.x,
+      this.origin.y,
+      this.origin.z - PETAL_DIST_FROM_ORIGIN,
+    );
+    const numPetals = this.positions.length / 3;
+    const angleIncrement = Math.PI / numPetals;
+    this.rotations = Array.from(Array(numPetals)).map(
+      (_, i) => i * angleIncrement,
+    );
   }
 
   update() {}
--- src/webgl/shaders/flower.frag
+++ src/webgl/shaders/flower.frag
@@ -8,8 +8,9 @@ varying float vAlpha;
 void main() {
     vec2 coords = (gl_PointCoord - 0.5) * mat2(vAngle.x, vAngle.y, -vAngle.y, vAngle.x) + 0.5;
     // vec2 coords = (gl_PointCoord - 0.5) + 0.5;
-    vec4 texture = texture2D(diffuseTexture, coords);
-    vec3 color = vec3(1.0, 1.0, 1.0);
+//     vec4 texture = texture2D(diffuseTexture, coords);
+//     vec3 color = vec3(1.0, 1.0, 1.0);
 
-    gl_FragColor = vec4(color, vAlpha * texture.a);
+//     gl_FragColor = vec4(color, vAlpha * texture.a);
+    gl_FragColor = vec4(1.0, 0.8, 0.8, 1.0);
 }
