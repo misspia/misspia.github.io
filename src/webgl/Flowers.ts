87203ecc62a9c0dc828b364deaaa064c476399f4
misspia <pia_leung@hotmail.com>
2024-06-23 04:33:52 -0400

    update flower texture, test alpha fade

--- src/webgl/Flowers.ts
+++ src/webgl/Flowers.ts
@@ -4,7 +4,11 @@ import vertexShader from "@webgl/shaders/flower.vert";
 import fragmentShader from "@webgl/shaders/flower.frag";
 import { toRadians, randomFloatBetween, remap } from "@utils";
 
-const NUM_FLOWERS = 500;
+const distanceFromCenter = (coordA: [number, number], coordB: [number, number]) => {
+  return Math.sqrt(Math.pow((coordA[0] - coordB[0]), 2) + Math.pow((coordA[1] - coordB[1]), 2))
+}
+
+const NUM_FLOWERS = 2000;
 
 const MIN_X_POS = -5;
 const MAX_X_POS = 5;
@@ -12,14 +16,20 @@ const MIN_Z_POS = 0;
 const MAX_Z_POS = 3.8;
 
 const MIN_SIZE = 1;
-const MAX_SIZE = 50;
+const MAX_SIZE = 30;
+
+
+const MAX_FADE_RADIUS_THRESHOLD = distanceFromCenter([0, 0], [MAX_X_POS, MAX_Z_POS])
+const MIN_FADE_RADIUS_THRESHOLD = MAX_FADE_RADIUS_THRESHOLD * 0.5
 
 export class Flowers {
+  origin: THREE.Vector3;
   geometry: THREE.BufferGeometry;
   material: THREE.Material;
   group: THREE.Points;
 
   constructor() {
+    this.origin = new THREE.Vector3();
     this.geometry = new THREE.BufferGeometry();
     this.material = new THREE.RawShaderMaterial({
       vertexShader,
@@ -44,17 +54,27 @@ export class Flowers {
   createFlowers() {
     const positions: number[] = [];
     const sizes: number[] = [];
+    const alphas: number[] = [];
 
     for (let i = 0; i < NUM_FLOWERS; i++) {
-      const origin = new THREE.Vector3(
+      const position = new THREE.Vector3(
         randomFloatBetween(MIN_X_POS, MAX_X_POS),
         0,
         randomFloatBetween(MIN_Z_POS, MAX_Z_POS),
       );
-      const size = remap(MIN_Z_POS, MAX_Z_POS, MIN_SIZE, MAX_SIZE, origin.z);
+      const size = remap(MIN_Z_POS, MAX_Z_POS, MIN_SIZE, MAX_SIZE, position.z);
 
-      positions.push(origin.x, origin.y, origin.z);
+      positions.push(position.x, position.y, position.z);
       sizes.push(size);
+
+      const distance = distanceFromCenter([this.origin.x, this.origin.z], [position.x, position.z]);
+      if(distance >= MIN_FADE_RADIUS_THRESHOLD) {
+        const alpha = remap(MIN_FADE_RADIUS_THRESHOLD, MAX_FADE_RADIUS_THRESHOLD, 0.0, 0.5, distance)
+        console.debug(alpha, distance, position)
+        alphas.push()
+      } else {
+        alphas.push(1)
+      }
     }
     this.geometry.setAttribute(
       "position",
@@ -64,7 +84,15 @@ export class Flowers {
       "size",
       new THREE.Float32BufferAttribute(sizes, 1),
     );
+    this.geometry.setAttribute(
+      "alpha",
+      new THREE.Float32BufferAttribute(sizes, 1),
+    );
   }
+
+
+  
+  
 
   update() {}
 }
--- src/webgl/shaders/flower.frag
+++ src/webgl/shaders/flower.frag
@@ -8,7 +8,8 @@ varying float vAlpha;
 void main() {
     vec2 coords = (gl_PointCoord - 0.5) * mat2(vAngle.x, vAngle.y, -vAngle.y, vAngle.x) + 0.5;
     vec4 texture = texture2D(diffuseTexture, coords);
-    vec4 color = vec4(5.0, 3.0, 3.0, 1.0);
+    // vec4 color = vec4(2.0, 2.0, 2.0, vAlpha);
+    vec4 color = vec4(2.0, 2.0, 2.0, vAlpha);
 
     gl_FragColor = color * texture;
 }
